3722e4134a5ac3f5a1c290fe1230192097fa83f7
Rskri2 <renukasinha1515@gmail.com>
2024-06-11 22:12:51 +0530

    Adding serve

---
+++ Servers/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan')
+const dotenv = require('dotenv');
+dotenv.config({ path:'./config.env' });
+
+const app = require('./app');
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+    console.log('listening to th port',PORT);
+})
